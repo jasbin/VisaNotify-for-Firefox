834b262d5dab6ce65a3598ad71d9185f31ab8599
jasbin <jasbinkarki@gmail.com>
2022-05-22 07:20:15 +0545

    notification details added in popup

--- content.js
+++ content.js
@@ -1,18 +1,30 @@
-// import {notify} from './logic.js'
 let timer;
 let errorMsg = 'Cannot get appointment date may be because account is not setup or may because of account suspension! please increase time interval for account ban issues!';
 
-// receive message from popup.checkInterval.js
-function gotMessage(message, sender, sendResponse){
-    localStorage.setItem('timer',message.time);
-    alert('Timer set to '+localStorage.getItem('timer') + ' seconds!');
-    timer = message.time;
-    let timeout = setTimeout(() => {
-        window.location.reload();
-    }, timer * 1000);
-}
-chrome.runtime.onMessage.addListener(gotMessage);
+// receive message from popup.js
+function gotMessage(message, sender, sendResponse) {
+    // check when set button is clicked
+    if (message.type === 'timer') {
+        localStorage.setItem('timer', message.time);
 
+        timer = message.time;
+
+        // sends response to popup.js
+        sendResponse({ timer: timer, appointmentDate: localStorage.getItem('appointmentDate') ?? 'Waiting For Appointment Date...' });
+
+        let timeout = setTimeout(() => {
+            window.location.reload();
+        }, timer * 1000);
+
+        alert('Timer set to ' + localStorage.getItem('timer') + ' seconds!');
+    }
+    // check when popup is opened
+    if (message.type === 'appointmentDate') {
+        sendResponse({ timer: timer, appointmentDate: localStorage.getItem('appointmentDate') ?? 'Waiting For Appointment Date...' });
+    }
+}
+
+chrome.runtime.onMessage.addListener(gotMessage);
 
 // set timer value
 timer = (localStorage.getItem('timer') ?? 30) * 1000; //default value as 30 sec if null initially
@@ -20,7 +32,7 @@ timer = (localStorage.getItem('timer') ?? 30) * 1000; //default value as 30 sec 
 // get appointment date
 let node = document.getElementsByClassName('leftPanelText')[0];
 
-let newAppointmentDate=null;
+let newAppointmentDate = null;
 
 try {
     newAppointmentDate = node.innerText;
--- popup.js
+++ popup.js
@@ -1,49 +1,65 @@
 // document.getElementById('timeInterval').value = localStorage.getItem('timer');
 let t = document.getElementsByClassName('time')[0].value = localStorage.getItem('time') ?? 30;
 document.getElementById('timerDetails').innerHTML = "<p>Timer Set to <span style='color:blue'><strong>" + t + "</strong></span> seconds</p>";
-
+document.getElementById('appointmentDate').innerHTML = localStorage.getItem('appointmentDate');
 
 let allowedUrl = [
    'https://cgifederal.secure.force.com/applicanthome',
    'https://cgifederal.secure.force.com/ApplicantHome'
 ];
 
+
+// check current tab url
+
 chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true, 'currentWindow': true }, function (tabs) {
    let currentUrl = tabs[0].url;
-
    if (currentUrl === allowedUrl[0] || currentUrl === allowedUrl[1]) {
-   
-      let btn = document.getElementsByClassName('set')[0];
-      btn.disabled = false;
-      btn.addEventListener('click', popup);
-      let time = document.getElementsByClassName('time')[0];
-      time.disabled = false;
-      
-      function popup(e) {
-         
-         if (isNaN(time.value)) {
-            alert('Please enter time in seconds');
-         } else {
+      initialize(tabs);
+   }
 
-            timeInSeconds = {
-               time: time.value ?? 30 // if time value is null set default value to 30 seconds
-            }
-            // alert(timeInSeconds);
-            let params = {
-               active: true,
-               currentWindow: true
-            }
-            //sends query to content_scripts
-            chrome.tabs.query(params, gotTab);
-            function gotTab(tabs) {
-               chrome.tabs.sendMessage(tabs[0].id, timeInSeconds);
-            }
-            localStorage.setItem('time', time.value);
+   chrome.tabs.sendMessage(tabs[0].id, { type: 'appointmentDate' }, function (response) {
+      let t = document.getElementsByClassName('time')[0].innerText = response.timer ?? localStorage.getItem('timer');
+      document.getElementById('appointmentDate').innerHTML = response.appointmentDate;
+      localStorage.setItem('appointmentDate', response.appointmentDate);
+   });
+});
+
+function initialize(tabs) {
+   let btn = document.getElementsByClassName('set')[0];
+   btn.disabled = false;
+   btn.addEventListener('click', popup);
+   let time = document.getElementsByClassName('time')[0];
+   time.disabled = false;
+
+   function popup(e) {
+
+      if (isNaN(time.value)) {
+         alert('Please enter time in seconds');
+      } else {
+
+         timeInSeconds = {
+            type: 'timer',
+            time: time.value ?? 30 // if time value is null set default value to 30 seconds
          }
+
+         let params = {
+            active: true,
+            currentWindow: true
+         }
+
+         //sends query to content_scripts
+         // chrome.tabs.query(params, gotTab);
+         // function gotTab(tabs) {
+
+         // }
+         chrome.tabs.sendMessage(tabs[0].id, timeInSeconds, function (response) {
+            let t = document.getElementsByClassName('time')[0].value = response.timer;
+            localStorage.setItem('time', response.timer);
+         });
       }
    }
 
-});
+}
 
 
 
